06e0a9714fa84515bd6b49ac85eac3feb6c9788c
李航 <864217697@qq.com>
2022-11-24 19:27:35 +0800

    添加毛玻璃效果

--- src/views/editpage/components/setting.ts
+++ src/views/editpage/components/setting.ts
@@ -4,7 +4,7 @@
  * @Autor: Your Name
  * @Date: 2022-11-24 11:21:17
  * @LastEditors: Your Name
- * @LastEditTime: 2022-11-24 14:46:45
+ * @LastEditTime: 2022-11-24 19:26:37
  */
 const defaultDom = {
   title: '',
@@ -12,20 +12,21 @@ const defaultDom = {
   color: '',
   style: {
     width: '80%',
-    maxWidth:'300px',
+    maxWidth: '300px',
     height: '180px',
     background: 'rgba(19,203,203,0.5)',
-    margin:'6px',
-    borderRadius:'6px',
-    border:'1px solid rgb(19,203,203)',
-    overflow:'hidden',
-    title:{
-      padding:'8px',
-      color:'#fff',
-      width:'100%',
+    margin: '6px',
+    borderRadius: '6px',
+    border: '1px solid rgb(19,203,203)',
+    overflow: 'hidden',
+    backdropFilter: 'saturate(100%) blur(10px)',
+    title: {
+      padding: '8px',
+      color: '#fff',
+      width: '100%',
       background: 'rgba(19,203,203,0.2)',
     }
-    // backdropFilter: 'saturate(100%) blur(10px)'
+
   }
 }
 export { defaultDom }
